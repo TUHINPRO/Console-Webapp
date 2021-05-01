e3fb3ef3180fe702a505aeb450c931b1a7026617
TUHINPRO <71925866+TUHINPRO@users.noreply.github.com>
2021-05-01 10:16:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var first_name= "Tuhin";
+console.log(first_name);
+
+var last_name= "Biswas";
+console.log(last_name);
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var length_of_full_name=full_name.length;
+console.log(length_of_full_name);
+
+var name_one= "Charlie Rutherford";
+console.log(name_one);
+var replace_name=name_one.replace("Rutherford","Singh");
+console.log(replace_name);
+
+var capital_name="TUHIN";
+console.log(capital_name);
+
+var making_lowercase= capital_name.toLowerCase();
+console.log(making_lowercase);
+
+var square_root=Math.sqrt(16);
+console.log(square_root);
